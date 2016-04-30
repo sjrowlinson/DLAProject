9320a85729c0aa31b9ec1aea7edc6efe16a73cc8
Samuel Rowlinson <sjrowlinson@outlook.com>
2016-04-30 01:44:08 +0100

    Updated managed c++ containers for 2d and 3d DLAs

--- DLAClassLibrary/DLAClassLibrary.h
+++ DLAClassLibrary/DLAClassLibrary.h
@@ -21,23 +21,59 @@ namespace DLAClassLibrary {
 
 	public:
 
-		ManagedDLA2DContainer() : native_DLA_container_ptr(new DLA_2d()) {
+		ManagedDLA2DContainer() : native_DLA_container_ptr(new DLA_2d()) {}
 
-		}
+		ManagedDLA2DContainer(double _coeff_stick) : native_DLA_container_ptr(new DLA_2d(_coeff_stick)) {}
 
-		ManagedDLA2DContainer(double _coeff_stick) : native_DLA_container_ptr(new DLA_2d(_coeff_stick)) {
+		ManagedDLA2DContainer(LatticeType _lattice_type, AttractorType _attractor_type) : native_DLA_container_ptr(new DLA_2d(_lattice_type, _attractor_type)) {}
 
-		}
-
-		ManagedDLA2DContainer(LatticeType _lattice_type, AttractorType _attractor_type) : native_DLA_container_ptr(new DLA_2d(_lattice_type, _attractor_type)) {
-
-		}
-
-		ManagedDLA2DContainer(LatticeType _lattice_type, AttractorType _attractor_type, double _coeff_stick) : native_DLA_container_ptr(new DLA_2d(_lattice_type, _attractor_type, _coeff_stick)) {
-
-		}
+		ManagedDLA2DContainer(LatticeType _lattice_type, AttractorType _attractor_type, double _coeff_stick) : native_DLA_container_ptr(new DLA_2d(_lattice_type, _attractor_type, _coeff_stick)) {}
 
 		ManagedDLA2DContainer() {
+			delete native_DLA_container_ptr;
+		}
+
+		double get_coeff_stick() {
+			return native_DLA_container_ptr->get_coeff_stick();
+		}
+
+		void set_coeff_stick(const double& _coeff_stick) {
+			native_DLA_container_ptr->set_coeff_stick(_coeff_stick);
+		}
+
+		size_t size() {
+			return native_DLA_container_ptr->size();
+		}
+
+		void clear() {
+			native_DLA_container_ptr->clear();
+		}
+
+		void generate(size_t _n) {
+			native_DLA_container_ptr->generate(_n);
+		}
+
+		double estimate_fractal_dimension() {
+			native_DLA_container_ptr->estimate_fractal_dimension();
+		}
+
+	};
+
+	public ref class ManagedDLA3DContainer {
+
+		DLAContainer* native_DLA_container_ptr;
+
+	public:
+
+		ManagedDLA3DContainer() : native_DLA_container_ptr(new DLA_3d()) {}
+
+		ManagedDLA3DContainer(double _coeff_stick) : native_DLA_container_ptr(new DLA_3d(_coeff_stick)) {}
+
+		ManagedDLA3DContainer(LatticeType _lattice_type, AttractorType _attractor_type) : native_DLA_container_ptr(new DLA_3d(_lattice_type, _attractor_type)) {}
+
+		ManagedDLA3DContainer(LatticeType _lattice_type, AttractorType _attractor_type, double _coeff_stick) : native_DLA_container_ptr(new DLA_3d(_lattice_type, _attractor_type, _coeff_stick)) {}
+
+		~ManagedDLA3DContainer() {
 			delete native_DLA_container_ptr;
 		}
 
